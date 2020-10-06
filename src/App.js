a891434c4fad0552dcc868aedbd41b67504fe58c
Damien Minter <Damo@MacBook-Pro-2.local>
2020-10-06 17:12:22 +1100

    Move initial state into own file

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ import DisplayTodo from "./componenets/DisplayTodo";
 import Header from "./componenets/Header";
 import ListTodo from "./componenets/ListTodo";
 import { useSelector } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
-
-// Get initial state from redux data
-const initialState = {
-  todos: [
-    {
-      id: uuidv4(),
-      name: "dummy todo 1",
-      complete: false,
-    },
-    {
-      id: uuidv4(),
-      name: "dummy todo 2",
-      complete: false,
-    },
-    {
-      id: uuidv4(),
-      name: "dummy todo 3",
-      complete: true,
-    },
-  ],
-  todo: { id: uuidv4(), name: "UseContext", complete: false },
-};
-console.log(initialState.todo);
+import { initialState } from "./context/data";
 
 // Create context and wrap App compontents in context provider and set value = initial state
 export const TodoContext = React.createContext(initialState);
