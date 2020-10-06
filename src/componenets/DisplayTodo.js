ef93e1fb92b5e4478689c0d1461e6c1bb8726c92
Damien Minter <Damo@MacBook-Pro-2.local>
2020-10-06 17:59:47 +1100

    Remove console.log

--- src/componenets/DisplayTodo.js
+++ src/componenets/DisplayTodo.js
@@ -4,7 +4,6 @@ import { TodoContext } from "../App";
 export const DisplayTodo = () => {
   const context = useContext(TodoContext);
   const todo = context.todo;
-  console.log("Child ", todo);
   return (
     <>
       {todo && (
