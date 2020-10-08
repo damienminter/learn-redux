366be4eb1af8b6bf49ab3f616438951b0b176a6a
Damien Minter <Damo@MacBook-Pro-2.local>
2020-10-09 00:46:11 +1100

    remove console.log

--- src/componenets/ListTodo.js
+++ src/componenets/ListTodo.js
@@ -6,7 +6,6 @@ import { TodoContext } from "../context/Context";
 export const ListTodo = () => {
   const context = useContext(TodoContext);
   const todos = context.state.todos;
-  console.log(todos);
   return (
     <div>
       {todos && todos.map((todo) => <ToDoItem key={todo.id} todo={todo} />)}
